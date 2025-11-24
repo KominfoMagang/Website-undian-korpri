908869784725ae603fe127d7998704c78a8a267f
ShDhz <zombiephoenix43@gmail.com>
2025-11-24 21:23:59 +0700

    feat : Add presence page check NIP and upload selfie for member page view

---
+++ resources/views/components/peserta/banner.blade.php
@@ -0,0 +1,16 @@
+    <div class="relative mb-6">
+        <!-- Banner Image -->
+        <div class="w-full h-40 bg-gray-200 rounded-lg overflow-hidden">
+            <img src="static/images/bannerTest.svg" alt="Banner Korpri" class="w-full h-full object-cover">
+        </div>
+
+        <!-- Logo  -->
+        <div class="absolute -top-4 left-1/2 -translate-x-1/2 flex gap-4">
+            <div class="bg-white rounded-lg p-3 shadow-lg">
+                <img src="/static/images/logoKotaTasik.png" alt="Logo Kota Tasik" class="w-14 h-14 object-contain">
+            </div>
+            <div class="bg-white rounded-lg p-3 shadow-lg">
+                <img src="/static/images/logoKorpri.png" alt="Logo Korpri" class="w-14 h-14 object-contain">
+            </div>
+        </div>
+    </div>
